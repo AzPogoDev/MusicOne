295802b0b9c1129748035d11cb5f7ef9fec6afcb
AzPogoDev <henri@git.com>
2021-10-20 11:37:38 +0200

    Adding assets configuration

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
+// Import stylesheet
+
 import "../css/app.scss";
+import "../css/src/layout/header.scss"
+import "../css/src/layout/footer.scss"
+import "../css/src/main/index.scss"
+
+// Import scripts
+
+import "./src/layout/header.js"
+
