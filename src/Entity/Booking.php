e12b7e31134cd40b431b2b54ec1989d0848233d5
AzPogoDev <henri@git.com>
2021-10-20 13:33:34 +0200

    Entity adding

---
+++ src/Entity/Booking.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookingRepository::class)
+ */
+class Booking
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="bookings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Event::class, inversedBy="participants")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $event;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $serial;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $seat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getEvent(): ?Event
+    {
+        return $this->event;
+    }
+
+    public function setEvent(?Event $event): self
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+
+    public function getSerial(): ?string
+    {
+        return $this->serial;
+    }
+
+    public function setSerial(string $serial): self
+    {
+        $this->serial = $serial;
+
+        return $this;
+    }
+
+    public function getSeat(): ?int
+    {
+        return $this->seat;
+    }
+
+    public function setSeat(int $seat): self
+    {
+        $this->seat = $seat;
+
+        return $this;
+    }
+}
